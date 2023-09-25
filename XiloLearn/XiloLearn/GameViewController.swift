f5abcad6db6e7385ed7426dd7c25a78ca7efc445
alslemos <alexandre-ls@hotmail.com>
2023-09-25 15:52:47 -0300

    added the playNode func, where i set the switch for each note

--- XiloLearn/XiloLearn/GameViewController.swift
+++ XiloLearn/XiloLearn/GameViewController.swift
@@ -38,4 +38,37 @@ class GameViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
+    func playNode(node: SKNode){
+        switch node.name {
+        case "nodeA":
+            node.run(SKAction.playSoundFileNamed("a", waitForCompletion: true))
+            
+        case "nodeB":
+            node.run(SKAction.playSoundFileNamed("b", waitForCompletion: true))
+            
+        case "nodeC":
+            node.run(SKAction.playSoundFileNamed("c", waitForCompletion: true))
+            
+        case "nodeC2":
+            node.run(SKAction.playSoundFileNamed("c2", waitForCompletion: true))
+            
+        case "nodeD1":
+            node.run(SKAction.playSoundFileNamed("d1", waitForCompletion: true))
+      
+        case "nodeE1":
+            node.run(SKAction.playSoundFileNamed("e1", waitForCompletion: true))
+            
+        case "nodeF":
+            node.run(SKAction.playSoundFileNamed("f", waitForCompletion: true))
+            
+        case "nodeG":
+            node.run(SKAction.playSoundFileNamed("g", waitForCompletion: true))
+            
+        default:
+            node.run(SKAction.playSoundFileNamed("sweep", waitForCompletion: true))
+        }
+    }
+    
+    
 }
