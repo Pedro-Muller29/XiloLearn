f6e83ce621f19d92c2901899f73744e592245c16
Pedro Mezacasa Muller <pedro.mezzacasa@gmail.com>
2023-09-25 15:13:08 -0300

    project setup

---
+++ XiloLearn/XiloLearn/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  XiloLearn
+//
+//  Created by Pedro Mezacasa Muller on 25/09/23.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // code here
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // code here
+    }
+}
